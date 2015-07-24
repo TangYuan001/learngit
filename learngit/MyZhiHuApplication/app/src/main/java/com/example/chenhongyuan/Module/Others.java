4536e73029549859a375fea3e0960e43906ca504
chenhongyuan <1837015019@qq.com>
2015-07-24 18:13:32 +0800

    add learngit/

---
+++ learngit/MyZhiHuApplication/app/src/main/java/com/example/chenhongyuan/Module/Others.java
@@ -0,0 +1,12 @@
+package com.example.chenhongyuan.Module;
+
+/**
+ * Created by chenhongyuan on 15/7/10.
+ */
+public class Others {
+    public int color;
+    public String thumbnail;
+    public String description;
+    public int id;
+    public String name;
+}
---
+++ learngit/README.md
@@ -0,0 +1,4 @@
+# learngit
+This is a project called MyZhiHuApplication.
+It is similar to ZhiHuRiBao.
+The remaining bags are about PGP.
